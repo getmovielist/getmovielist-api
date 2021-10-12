47cf73f7ad56489414029bd01dca04d85c8e8a85
Jefferson Uchôa Ponte <jefponte@gmail.com>
2021-10-12 13:27:45 -0300

    Create lume/laravel application

---
+++ app/Http/Controllers/TokenController.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Http\Controllers;
+use Firebase\JWT\JWT;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+class TokenController extends Controller
+{
+    public function generateToken(Request $request)
+    {
+
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        $usuario = User::where('email', $request->email)->first();
+
+        if (is_null($usuario)
+            || !Hash::check($request->password, $usuario->password)
+        ) {
+            return response()->json('Invalid user', 401);
+        }
+
+        $token = JWT::encode(
+            ['email' => $request->email],
+            env('JWT_KEY')
+        );
+
+        return [
+            'access_token' => $token
+        ];
+    }
+}
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Firebase\JWT\JWT;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Boot the authentication services for the application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app['auth']->viaRequest('api', function (Request $request) {
+            if (!$request->hasHeader('Authorization')) {
+                return null;
+            }
+            $authorizationHeader = $request->header('Authorization');
+            $token = str_replace('Bearer ', '', $authorizationHeader);
+            $dadosAutenticacao = JWT::decode($token, env('JWT_KEY'), ['HS256']);
+
+            return User::where('email', $dadosAutenticacao->email)
+                 ->first();
+        });
+    }
+}
